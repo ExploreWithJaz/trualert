0d57059836db6db29eb634d2756e69f995f1ccc2
Jazper-Fullstack <jazper@fullstack.ph>
2025-06-16 18:38:16 +0800

    style: improve layout and typography for better readability on Home page

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -5,13 +5,17 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      <div className='mt-20 px-2'>
-        <section className='flex flex-col mx-80 py-2.5 gap-5 text-center'>
-          <div className='text-[80px]'>
-            <h1>EMERGENCY AID, JUST A <span>TAP AWAY</span></h1>
+            <div className='mt-20 px-2'>
+        <section className='flex flex-col items-center max-w-[850px] mx-auto py-2.5 gap-5 text-center'>
+          <div className='text-[80px] font-bold w-full'>
+            <h1>
+              EMERGENCY AID, JUST A <span className='text-[#ff0f0f]'>TAP AWAY</span>
+            </h1>
           </div>
-          <div className='text-lg'>
-            <p>Experience peace of mind knowing help is just a tap away. TRUalert instantly connects you to professional emergency dispatchers via Noonlight—no tracking, no delays, just real help when you need it most.</p>
+          <div className='text-lg w-full'>
+            <p>
+              Experience peace of mind knowing help is just a tap away. TRUalert instantly connects you to professional emergency dispatchers via Noonlight—no tracking, no delays, just real help when you need it most.
+            </p>
           </div>
           <div className='flex justify-center items-center gap-4'>
             <img className='w-[100px]' src="/app-store-download.svg" alt="App Store Download Button" />
@@ -20,7 +24,7 @@ const Home = () => {
           <div>
             <a href="">
               <button>Button</button>
-              </a>
+            </a>
           </div>
         </section>
       </div>
