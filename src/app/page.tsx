cb2314781d6e89f211a167ede4ce7424b1d6b674
Jazper-Fullstack <jazper@fullstack.ph>
2025-06-17 10:37:13 +0800

    style: update typography and layout for improved readability on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,19 @@ function page() {
       <Navigation />
       <div className='mt-20 px-2'>
         <section className='flex flex-col items-center max-w-[850px] mx-auto py-2.5 gap-5 text-center'>
-          <div className='text-[80px] font-bold w-full'>
+          <div className='text-[80px]/23 font-bold w-full'>
             <h1>
               EMERGENCY AID, JUST A <span className='text-[#ff0f0f]'>TAP AWAY</span>
             </h1>
           </div>
-          <div className='text-lg w-full'>
-            <p>
+          <div className='text-lg w-[588.8px] h-auto mb-3.5 '>
+            <p className='text-white text-lg text-center leading-7 tracking-wider'>
               Experience peace of mind knowing help is just a tap away. TRUalert instantly connects you to professional emergency dispatchers via Noonlight—no tracking, no delays, just real help when you need it most.
             </p>
           </div>
           <div className='flex justify-center items-center gap-4'>
             <img className='w-[100px]' src="/app-store-download.svg" alt="App Store Download Button" />
-            <img className='w-[100px]' src="/google-play-button.svg" alt="Google Play Download Button" />
+            <img className='w-[125px]' src="/google-play-button.svg" alt="Google Play Download Button" />
           </div>
           <div>
             <a href="">
