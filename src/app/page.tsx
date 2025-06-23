647070db9e6d6ac16638b9011842a67bb3f8ba10
Jazper-Fullstack <jazper@fullstack.ph>
2025-06-23 10:23:12 +0800

    Update dependencies and adjust background styles
    Adds `@types/aos` to devDependencies and updates `next` version.
    Comments out CSS background styles for better readability and
    implements a new inverted background for the main page.
    
    Improves maintainability by refining styles and dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,16 @@ function page() {
   return (
     <>
       <Navigation />
+      <div
+  className="fixed inset-0 w-full h-full min-h-screen z-[-1] bg-cover bg-center"
+  style={{
+    backgroundImage: "url('/map.png')",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    filter: 'invert(1) brightness(0.8) contrast(1.1) saturate(0.7) hue-rotate(180deg)',
+  }}
+></div>
       <div className='mt-20 px-2'>
         <section className='flex flex-col items-center max-w-[850px] mx-auto py-2.5 gap-5 text-center'>
           <div className='text-[80px]/21 font-bold w-full'>
