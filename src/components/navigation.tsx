1a7fac6341d831e0a79acb877ab773e6bd69b973
Jazper-Fullstack <jazper@fullstack.ph>
2025-06-28 17:59:44 +0800

    Refactor navigation component for improved layout and responsiveness

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,8 +5,9 @@ function navigation() {
 
   return (
     <div>
-      <div className='hidden xl:block items-center mx-20 w-auto font-sans font-medium'>
-        <section className='flex justify-between items-center mx-60 py-8'>
+      {/* Desktop Navigation */}
+      <div className='hidden lg:block items-center mx-20 w-auto font-sans font-medium'>
+        <section className='flex justify-between items-center mx-auto max-w-[1280px] py-8'>
           <div className='flex items-center gap-2'>
             <a href="/">
             <img className='w-12' src="https://trualert.com/wp-content/uploads/2024/08/LogoMark-Dark-Mode.svg" alt="Trualert Logo" />
@@ -31,8 +32,8 @@ function navigation() {
           </div>
         </section>
       </div>
-
-      <div className='flex xl:hidden w-full px-5 md:px-10 relative'>
+      {/* Mobile Navigation */}
+      <div className='flex lg:hidden w-full px-5 md:px-10 relative'>
         <section className='flex flex-row justify-between items-center py-7.5 w-full'>
           <div className='flex items-center gap-1.5 md:gap-5'>
             <img className='w-[34px] md:w-[48px] h-auto' src="https://trualert.com/wp-content/uploads/2024/08/LogoMark-Dark-Mode.svg" alt="Trualert Logo" />
